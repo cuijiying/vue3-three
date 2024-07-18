63ede5a94ae58a160cb1c99283077edff7b38f3b
cuijiying <1422077390@qq.com>
2024-07-18 23:04:43 +0800

    多材质使用

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,29 +18,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// 创建几何体
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// 创建材质
-const parentMaterial = new THREE.MeshBasicMaterial({ color: 0x00ffff });
-parentMaterial.wireframe = true
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-
-const parent = new THREE.Mesh(geometry, parentMaterial);
-parent.position.set(-2, 0, 0)
-// parent.scale.set(2, 2, 2)
-// 创建网格
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(2, 0, 0)
-cube.rotation.set(Math.PI / 4, 0, 0)
-// 缩放
-cube.scale.set(2, 2, 2)
-parent.rotation.set(Math.PI / 4, 0, 0)
-parent.add(cube)
-
-// 添加到场景
-scene.add(parent)
+// 窗口大小改变自动调整
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 // 坐标辅助器
 const axesHelper = new THREE.AxesHelper(5);
@@ -51,55 +34,6 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;  // 启用阻尼
 controls.dampingFactor = 0.05;  // 阻尼系数
 
-// 窗口大小改变自动调整
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-// 点击按钮全屏
-let fullScreen = document.createElement('button');
-fullScreen.innerHTML = '全屏';
-document.body.appendChild(fullScreen);
-fullScreen.onclick = () => {
-  const fullScreenEL = document.fullscreenElement;
-  if (!fullScreenEL) {
-    fullScreen.innerHTML = '退出全屏';
-    document.body.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-    fullScreen.innerHTML = '全屏';
-  }
-}
-
-// gui
-const gui = new GUI();
-const obj = {
-  isFullScreen: false,
- 
-  // 位置
-}
-gui.add(obj, 'isFullScreen').onChange(() => {
-  if (obj.isFullScreen) {
-    fullScreen.click();
-  } else {
-    fullScreen.click();
-  }
-});
-gui.addColor(parentMaterial, 'color').onChange(() => {
-  parentMaterial.color.set(parentMaterial.color);
-});
-gui.add(parent.position, 'x', -5, 5);
-// gui.add(cube.position, 'y').min(-5).max(5);
-gui.add(parent.position, 'y').min(-5).max(5);
-gui.add(parent.position, 'z').min(-5).max(5).name("z轴").step(0.1);
-// 设置线框模式
-gui.add(parentMaterial, 'wireframe').name("是否线框材质").onChange((value) => {
-  parentMaterial.wireframe = value;
-});
-
-
 function animate(){
   controls.update();  // 更新轨道控制器
   requestAnimationFrame(animate);
@@ -107,6 +41,90 @@ function animate(){
 }
 
 animate();
+
+
+
+// 创建几何体
+const geometry = new THREE.BufferGeometry();
+console.log(geometry, 'geometry');
+
+const vertices = new Float32Array([
+  -1.0, -1.0, 0,
+  1.0, -1.0, 0,
+  1.0, 1.0, 0,
+  // 1.0, 1.0, 0,
+  -1.0, 1.0, 0,
+  // -1.0, -1.0, 0,
+]);
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+const material0 = new THREE.MeshBasicMaterial({ 
+  color: 0x00ff00, 
+  // wireframe: true,
+  side: THREE.DoubleSide
+});
+const material1 = new THREE.MeshBasicMaterial({ 
+  color: 0xff0000, 
+  // wireframe: true,
+  // side: THREE.DoubleSide
+});
+// 创建索引
+const indices = new Uint16Array([
+  0, 1, 2,
+  2, 3, 0
+]);
+geometry.addGroup(0, 3, 0);
+geometry.addGroup(3, 3, 1);
+geometry.setIndex(new THREE.BufferAttribute(indices, 1));
+const cubeMy = new THREE.Mesh(geometry, [material0, material1]);
+scene.add(cubeMy);
+
+// // 创建几何体
+const cubegeometry = new THREE.BoxGeometry(1, 1, 1);
+console.log(cubegeometry);
+// // 创建材质
+const cubematerial0 = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  // wireframe: true,
+});
+const cubematerial1 = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+});
+const cubematerial2 = new THREE.MeshBasicMaterial({
+  color: 0x0000ff,
+});
+const cubematerial3 = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+});
+const cubematerial4 = new THREE.MeshBasicMaterial({
+  color: 0x00ffff,
+});
+const cubematerial5 = new THREE.MeshBasicMaterial({
+  color: 0xff00ff,
+});
+const cube = new THREE.Mesh(cubegeometry, [
+  cubematerial0,
+  cubematerial1,
+  cubematerial2,
+  cubematerial3,
+  cubematerial4,
+  cubematerial5,
+]);
+// console.log(geometry);
+
+cube.position.x = 2;
+
+// 将网格添加到场景中
+scene.add(cube);
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <template>
